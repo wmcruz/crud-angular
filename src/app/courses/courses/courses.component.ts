4ccfc7897ae6df65bbede9c4295c02b3ebd82740
Wellington Cruz <wellington.max1@gmail.com>
2025-03-23 08:45:52 -0300

    Criando service de courses e realizando as importações e injeção de depêndencia necessárias.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,14 +9,13 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'},
-    { _id: '2', name: 'Spring', category: 'back-end'}
-  ];
   displayedColumns = ['name', 'category'];
+  courses: Course[] = [];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) {
+  }
 
   ngOnInit(): void {
+    this.courses = this.coursesService.list();
   }
 }
