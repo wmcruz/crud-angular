540a7c0fd9c7427954cc78ff71d1bfc9e40581fa
Wellington Cruz <wellington.max1@gmail.com>
2025-11-24 22:36:24 -0300

    Features include creating, listing, posting, and error handling.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
-import { delay, take, tap } from 'rxjs/operators'
+import { delay, first, take, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +20,10 @@ export class CoursesService {
       delay(5000),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record)
+    .pipe(first());
   }
 }
