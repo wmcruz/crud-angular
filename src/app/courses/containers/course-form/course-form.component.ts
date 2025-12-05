6cae5c4de4810812ffd4178596d6f990db360641
Wellington Cruz <wellington.max1@gmail.com>
2025-12-04 21:49:44 -0300

    Populating the form.

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../../services/courses.service';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../model/course';
+import { core } from '@angular/compiler';
 
 @Component({
   selector: 'app-course-form',
@@ -12,6 +15,7 @@ import { CoursesService } from '../../services/courses.service';
 export class CourseFormComponent implements OnInit {
 
     form = this.formBuilder.group({
+      _id: [''],
       name: (''),
       category: ('')
     });
@@ -20,12 +24,17 @@ export class CourseFormComponent implements OnInit {
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location
-  ) {
+    private location: Location,
+    private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.form.value.name = 'Loiane';
+    const course: Course = this.route.snapshot.data['course'];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    })
   }
 
   onSubmit(): void {
--- src/app/courses/guards/course.resolver.ts
+++ src/app/courses/guards/course.resolver.ts
@@ -15,7 +15,10 @@ export class CourseResolver implements Resolve<Course> {
 
   constructor(private service: CoursesService) {}
 
-
+  /**
+   * Um resolver sempre é executado antes de carregar o component.
+   * Esse tipo de abordagem só é indicado se o carregamento for rapido.
+   */
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
     if (route.params && route.params['id']) {
       return this.service.loadById(route.params['id']);
