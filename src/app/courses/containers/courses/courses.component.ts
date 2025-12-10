0a1fb4cdb75d68118a6acf63bf37b08807c3906d
Wellington Cruz <wellington.max1@gmail.com>
2025-12-09 21:47:33 -0300

    Add option of delete course.

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
-import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -15,14 +16,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CoursesComponent implements OnInit {
 
-  courses$: Observable<Course[]>;
+  courses$: Observable<Course[]> | null = null;
 
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
   ) {
+    this.refresh();
+  }
+
+  refresh () {
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
@@ -48,5 +54,18 @@ export class CoursesComponent implements OnInit {
 
   onEdit(course: Course) {
     this.router.navigate(['edit', course._id], {relativeTo: this.route});
+  }
+
+  onRemove(course: Course) {
+    this.coursesService.remove(course._id)
+    .subscribe(() => {
+      this.refresh();
+      this.snackBar.open('Curso removido com sucesso!', 'X', {
+        duration: 5000,
+        verticalPosition: 'top',
+        horizontalPosition: 'center'
+      });
+    },
+    error => this.onError('Erro ao tentar remover curso.'));
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -42,4 +42,9 @@ export class CoursesService {
     return this.httpClient.put<Course>(`${this.API}/${record._id}`, record)
       .pipe(first());
   }
+
+  remove(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`)
+      .pipe(first());
+  }
 }
